efd638a00483f645f7fb264bec34d622d6f79a87
TaejeongYu <qmamsm123@naver.com>
2023-01-12 15:15:23 +0900

    Implement project in basic level

---
+++ src/hooks/useTimer.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export function useTimer() {
+    const [time, setTime] = useState<number>(0);
+    const [onGoing, setOnGoing] = useState<boolean>(false);
+    const [intervalID, setIntervalID] = useState<NodeJS.Timer | undefined>(undefined);
+
+    const stopTimer = () => {
+        if (intervalID) {
+            clearInterval(intervalID);
+            setIntervalID(undefined);
+        }
+        setOnGoing(false);
+    };
+
+    const startTimer = () => {
+        stopTimer();
+        setIntervalID(setInterval(() => {
+            setTime((prevTime) => prevTime + 1);
+        }, 1000));
+        setOnGoing(true);
+    }
+
+    return {onGoing, time, startTimer, stopTimer};
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,15 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { RecoilRoot } from "recoil";
+import { Todo } from "./components/Todo";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RecoilRoot>
+    <div style={{textAlign: "center"}}>
+    <h1>ToDo List</h1>
+    <Todo />
+    </div>
+  </RecoilRoot>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
